f92eeae5b21325543317e943339bfd9455263940
Kiwifuit <mahkiwi123@gmail.com>
2024-10-22 20:41:21 +0800

    feat: hello world :D

--- dms-backend/src/main.rs
+++ dms-backend/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+use tokio::net::TcpListener;
+
+async fn index() -> &'static str {
+  "Hello world!"
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
+async fn main() {
+  let app = Router::new().route("/", get(index));
+
+  let server = TcpListener::bind("0.0.0.0:3030").await.unwrap();
+
+  axum::serve(server, app).await.unwrap();
 }
